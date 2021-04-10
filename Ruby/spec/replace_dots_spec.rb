ccc44290fd36fbf2a299bc18ee7694fa175c2ed1
xavierloos <xavier.loos04@gmail.com>
2021-04-10 12:01:48 +0100

    random

--- Ruby/spec/replace_dots_spec.rb
+++ Ruby/spec/replace_dots_spec.rb
@@ -13,4 +13,11 @@ describe "replaceDots" do
   it "works for all dots" do
     expect(replaceDots("...............")).to eq "---------------"
   end
+  it "works for random dots" do
+    expect(replaceDots("x...xx..x.xx...xxx.x")).to eq "x---xx--x-xx---xxx-x"
+    expect(replaceDots("xxxx.....x.....x.x..")).to eq "xxxx-----x-----x-x--"
+    expect(replaceDots("xx.xx...xx.x....x.x.")).to eq "xx-xx---xx-x----x-x-"
+    expect(replaceDots(".xx....x..x.xx.x..xx")).to eq "-xx----x--x-xx-x--xx"
+    expect(replaceDots("x.xx...x...xxxxx.xx.")).to eq "x-xx---x---xxxxx-xx-"
+  end
 end
