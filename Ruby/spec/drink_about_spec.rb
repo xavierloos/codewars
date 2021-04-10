0a661a5a916663021dea91b218b3b2d7fc6be898
xavierloos <xavier.loos04@gmail.com>
2021-04-10 11:18:11 +0100

    Kata tests passing

--- Ruby/spec/drink_about_spec.rb
+++ Ruby/spec/drink_about_spec.rb
@@ -11,6 +11,19 @@ describe "people_with_age_drink" do
     expect(people_with_age_drink(20)).to eq "drink beer" 
   end
   it "returns drink tody if minimun age" do
-    expect(people_with_age_drink(30)).to eq "drink wisky" 
+    expect(people_with_age_drink(30)).to eq "drink whisky" 
+  end
+  describe "Fixed tests" do
+    it "should pass fixed tests" do
+      expect(people_with_age_drink(22)).to eq 'drink whisky'
+      expect(people_with_age_drink(21)).to eq 'drink whisky'
+      expect(people_with_age_drink(20)).to eq 'drink beer'
+      expect(people_with_age_drink(18)).to eq 'drink beer'
+      expect(people_with_age_drink(17)).to eq 'drink coke'
+      expect(people_with_age_drink(15)).to eq 'drink coke'
+      expect(people_with_age_drink(14)).to eq 'drink coke'
+      expect(people_with_age_drink(13)).to eq 'drink toddy'
+      expect(people_with_age_drink(0)).to eq 'drink toddy'
+    end
   end
 end
