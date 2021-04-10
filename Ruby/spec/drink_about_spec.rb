8bc83c7afbe312037ab8b9db7657cc7ab5428a99
xavierloos <xavier.loos04@gmail.com>
2021-04-10 11:24:07 +0100

    Update

--- Ruby/spec/drink_about_spec.rb
+++ Ruby/spec/drink_about_spec.rb
@@ -4,13 +4,13 @@ describe "people_with_age_drink" do
   it "returns drink tody if minimun age" do
     expect(people_with_age_drink(13)).to eq "drink toddy" 
   end
-  it "returns drink tody if minimun age" do
+  it "returns drink coke if minimun age" do
     expect(people_with_age_drink(17)).to eq "drink coke" 
   end
-  it "returns drink tody if minimun age" do
+  it "returns drink beer if minimun age" do
     expect(people_with_age_drink(20)).to eq "drink beer" 
   end
-  it "returns drink tody if minimun age" do
+  it "returns drink whisky if minimun age" do
     expect(people_with_age_drink(30)).to eq "drink whisky" 
   end
   describe "Fixed tests" do
