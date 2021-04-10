190cea5b06f129009d8dc702a54645353fde1f72
xavierloos <xavier.loos04@gmail.com>
2021-04-10 11:55:12 +0100

    Spec

--- Ruby/lib/replace_dots.rb
+++ Ruby/lib/replace_dots.rb
@@ -1,3 +1,15 @@
+# https://www.codewars.com/kata/596c6eb85b0f515834000049/train/ruby
+
+# The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+# But it's not working properly.
+
+# Task
+# Fix the bug so we can all go home early.
+
+# Notes
+# String str will never be null.
+
 def replaceDots(str)
   str.gsub(/[.]/, '-')
 end
