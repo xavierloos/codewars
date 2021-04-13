3c0471281d2963ddc71dc99bd06f65a2895ce5ee
xavierloos <xavier.loos04@gmail.com>
2021-04-13 14:23:22 +0100

    Filter long words

---
+++ Ruby/lib/filter_long_words.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5697fb83f41965761f000052/train/ruby
+
+# Return a list of all words that are longer than n.
+
+def filter_long_words(sentence, n)
+  #your code here
+end
