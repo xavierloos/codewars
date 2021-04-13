58aac86ea2127a1faf78b68cde4ecb94168da338
xavierloos <xavier.loos04@gmail.com>
2021-04-13 15:44:16 +0100

    ABC to abc

--- Ruby/lib/alternate.rb
+++ Ruby/lib/alternate.rb
@@ -1,5 +1,7 @@
+# https://www.codewars.com/kata/57a62154cf1fa5b25200031e/train/ruby
 # Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 
 def alternate_case(s)
   #your code here
+  s.downcase
 end
