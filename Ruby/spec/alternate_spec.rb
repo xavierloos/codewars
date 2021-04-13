8e9420d4331c7d03d624f8076b311a1aec5d0518
xavierloos <xavier.loos04@gmail.com>
2021-04-13 15:51:24 +0100

    Additional test passed

--- Ruby/spec/alternate_spec.rb
+++ Ruby/spec/alternate_spec.rb
@@ -10,4 +10,12 @@ describe "alternate_case" do
   it "returns 'I like making katas very much'" do
     expect(alternate_case("i LIKE MAKING KATAS VERY MUCH")).to eq "I like making katas very much"
   end
+  describe "additional test" do
+    it "should pass all the test" do
+      expect(alternate_case("HuMpTy DuMpTy SaT On A WaLl")).to eq "hUmPtY dUmPtY sAt oN a wAlL"
+      expect(alternate_case("aBracaDabRa")).to eq "AbRACAdABrA"
+      expect(alternate_case("Hickory DICKORY dock")).to eq "hICKORY dickory DOCK"
+      expect(alternate_case("Jack JUMPED over THE CaNdLeStIcK")).to eq "jACK jumped OVER the cAnDlEsTiCk"
+    end 
+  end
 end
