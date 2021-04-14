033640c2eb9c7df5664dfcf65e964f85c2855f09
xavierloos <xavier.loos04@gmail.com>
2021-04-14 20:05:21 +0100

    Returns an ordered list

--- Ruby/lib/sort_numbers.rb
+++ Ruby/lib/sort_numbers.rb
@@ -7,4 +7,5 @@
 
 def solution(nums)
   # Your code here
+  nums.sort
 end
--- Ruby/spec/sort_numbers_spec.rb
+++ Ruby/spec/sort_numbers_spec.rb
@@ -2,7 +2,7 @@ require "sort_numbers"
 
 describe "solution" do
   it "shoul return an array in order" do
-    expect([1, 2, 3, 10, 5]).to eq [1, 2, 3, 5, 10]
+    expect(solution([1, 2, 3, 10, 5])).to eq [1, 2, 3, 5, 10]
   end
   
 end
