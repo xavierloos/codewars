e8e2de99f3e754407b2d5a8b130e349a86ebef17
xavierloos <xavier.loos04@gmail.com>
2021-04-14 19:33:40 +0100

    should handle 1

--- Ruby/spec/pre_fizzBuzz_workout_spec.rb
+++ Ruby/spec/pre_fizzBuzz_workout_spec.rb
@@ -4,5 +4,7 @@ describe "pre_fizz" do
   it "returns an Array" do
     expect(pre_fizz(1)).to be_a_kind_of(Array) 
   end
-  
+  it "should handle 1" do
+    expect(pre_fizz(1)).to eq [1] 
+  end
 end
