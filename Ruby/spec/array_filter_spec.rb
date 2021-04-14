773379b878471d10bd5e4635cab507e5c7bd4af9
xavierloos <xavier.loos04@gmail.com>
2021-04-14 21:32:27 +0100

    more test

--- Ruby/spec/array_filter_spec.rb
+++ Ruby/spec/array_filter_spec.rb
@@ -13,4 +13,10 @@ describe "get_even_numbers" do
   it "returns 2" do
     expect(get_even_numbers([1, 2])).to eq [2]
   end
+  describe "more test" do
+    it "returns even" do
+      expect(get_even_numbers([1,2,3,4,5])).to eq [2,4]
+      expect(get_even_numbers([2,4,6,8])).to eq [2,4,6,8]
+    end
+  end
 end
