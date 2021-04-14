7f53e7b732f2c32cb6cc3d9b1d8824a3ed6c1860
xavierloos <xavier.loos04@gmail.com>
2021-04-14 19:52:32 +0100

    should handle 10 test

--- Ruby/spec/pre_fizzBuzz_workout_spec.rb
+++ Ruby/spec/pre_fizzBuzz_workout_spec.rb
@@ -5,6 +5,9 @@ describe "pre_fizz" do
     expect(pre_fizz(1)).to be_a_kind_of(Array) 
   end
   it "should handle 1" do
-    expect(pre_fizz(1)).to eq [1] 
+    expect(pre_fizz(1)).to eq [1]
+  end
+  it "should handle 10" do
+    expect(pre_fizz(10)).to eq [1,2,3,4,5,6,7,8,9,10]
   end
 end
