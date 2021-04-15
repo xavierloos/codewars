ce7fc3c62259c7f32ee41b381210836a7d52a004
xavierloos <xavier.loos04@gmail.com>
2021-04-15 20:53:30 +0100

    country

--- Ruby/spec/remove_character_spec.rb
+++ Ruby/spec/remove_character_spec.rb
@@ -4,5 +4,8 @@ describe "remove_char" do
   it "deletes first and last" do
     expect(remove_char("eloquent")).to eq "loquen"
   end
+  it "deletes first and last" do
+    expect(remove_char('country')).to eq 'ountr'
+  end
   
 end
