0f3d7ac5c2907bf83c433e3f8e0bbbca456fb026
xavierloos <xavier.loos04@gmail.com>
2021-04-15 20:54:05 +0100

    Person

--- Ruby/spec/remove_character_spec.rb
+++ Ruby/spec/remove_character_spec.rb
@@ -7,5 +7,7 @@ describe "remove_char" do
   it "deletes first and last" do
     expect(remove_char('country')).to eq 'ountr'
   end
-  
+  it "deletes first and last" do
+    expect(remove_char('person')).to eq 'erso'
+  end
 end
