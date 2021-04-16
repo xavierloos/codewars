450d28e150520c8f3bb1ea369fcddc11ed7847ce
xavierloos <xavier.loos04@gmail.com>
2021-04-16 18:22:54 +0100

    returns false

--- Ruby/spec/anagram_spec.rb
+++ Ruby/spec/anagram_spec.rb
@@ -4,4 +4,7 @@ describe "isAnagram?" do
     it 'returns true if contains the same letters ' do
       expect(isAnagram?("Debit card", "Bad credit")).to eq true
     end
+    it 'returns true if contains the same letters ' do
+      expect(isAnagram?("Debit card", "Bacredit")).to eq false
+    end
 end
