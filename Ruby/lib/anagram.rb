8507a5df7b47c93e24aa540ae36146391c61a323
xavierloos <xavier.loos04@gmail.com>
2021-04-17 11:19:01 +0100

    Passing test

--- Ruby/lib/anagram.rb
+++ Ruby/lib/anagram.rb
@@ -1,4 +1,16 @@
 # Check is the 2 strings passed are anagrams
 def isAnagram?(str1, str2)
-  
+  count_letter = 0
+  str1 = str1.delete(' ') # Delete the spaces in the string
+  str2 = str2.delete(' ') # Delete the spaces in the string
+  str1.each_char do |l|
+    if str2.downcase.include?(l.downcase)
+      count_letter += 1
+    end
+  end
+  if count_letter == str2.length
+    return true
+  else
+    return false
+  end
 end
