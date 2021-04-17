3ef0f4a7593eca996b68d7c22b0d05932ef416af
xavierloos <xavier.loos04@gmail.com>
2021-04-17 11:22:10 +0100

    Update

--- Ruby/lib/anagram.rb
+++ Ruby/lib/anagram.rb
@@ -6,12 +6,13 @@ def isAnagram?(str1, str2)
   str1.each_char do |l|
     if str2.downcase.include?(l.downcase)
       count_letter += 1
+      
     end
   end
   if count_letter == str2.length
     return true
   else
-    
+
     return false
   end
 
