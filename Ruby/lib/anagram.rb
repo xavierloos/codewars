5297422c4ce4e1708de3f23bb8d8228dbd749d0e
xavierloos <xavier.loos04@gmail.com>
2021-04-17 11:27:06 +0100

    update

--- Ruby/lib/anagram.rb
+++ Ruby/lib/anagram.rb
@@ -1,9 +1,8 @@
 # Check is the 2 strings passed are anagrams
 def isAnagram?(str1, str2)
   count_letter = 0
-  str1 = str1.delete(' ') # Delete the spaces in the string
   str2 = str2.delete(' ') # Delete the spaces in the string
-  str1.each_char do |l|
+  str1.delete(' ').each_char do |l|
     count_letter += 1 if str2.downcase.include?(l.downcase)
   end
   count_letter == str2.length ?  true : false
