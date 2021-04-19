50338d0edd50f223a3f7f623683fb03fffb317f4
xavierloos <xavier.loos04@gmail.com>
2021-04-19 13:49:24 +0100

    Update

--- Ruby/spec/replace_dots_spec.rb
+++ Ruby/spec/replace_dots_spec.rb
@@ -4,7 +4,6 @@ describe "replaceDots" do
   it "replace all dots in the string" do
     expect(replaceDots("one.two.three")).to eq "one-two-three"
   end
-
   it "works for empty strings" do
     expect(replaceDots(" ")).to eq " "
   end
