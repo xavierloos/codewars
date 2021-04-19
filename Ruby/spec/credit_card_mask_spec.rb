8baf7f7cf6355c2276339ba0149ba642c696db74
xavierloos <xavier.loos04@gmail.com>
2021-04-19 12:47:54 +0100

    Adding test

--- Ruby/spec/credit_card_mask_spec.rb
+++ Ruby/spec/credit_card_mask_spec.rb
@@ -1,1 +1,8 @@
 require "credit_card_mask"
+
+describe "maskify" do
+  it "should replace characters for # and leave thee last 4 as it is" do
+    expect(maskify('4556364607935616')).to eq '############5616'
+  end
+  
+end
