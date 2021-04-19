c7f0c607c8ade7b35695ed101e6d2b0225117220
xavierloos <xavier.loos04@gmail.com>
2021-04-19 12:49:43 +0100

    return 1

--- Ruby/spec/credit_card_mask_spec.rb
+++ Ruby/spec/credit_card_mask_spec.rb
@@ -4,5 +4,7 @@ describe "maskify" do
   it "should replace characters for # and leave thee last 4 as it is" do
     expect(maskify('4556364607935616')).to eq '############5616'
   end
-  
+  it "should return 1 because it's not very long" do
+    expect(maskify('1')).to eq '1'
+  end
 end
