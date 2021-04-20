01dee0e1fbb396b0384dc67232f23435c45072de
xavierloos <xavier.loos04@gmail.com>
2021-04-20 21:19:41 +0100

    Note

--- Ruby/lib/generate_range_int.rb
+++ Ruby/lib/generate_range_int.rb
@@ -7,3 +7,8 @@
 
 # generate_range(2, 10, 2) # should return array of [2,4,6,8,10]
 # generate_range(1, 10, 3) # should return array of [1,4,7,10]
+
+# Note
+# min < max
+# step > 0
+# the range does not HAVE to include max (depending on the step)
