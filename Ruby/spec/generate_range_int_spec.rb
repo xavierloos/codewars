1549abbdb3b53f90e9cb7697fcc04c5b96c1e665
xavierloos <xavier.loos04@gmail.com>
2021-04-20 21:39:32 +0100

    any start point, any step

--- Ruby/spec/generate_range_int_spec.rb
+++ Ruby/spec/generate_range_int_spec.rb
@@ -7,4 +7,7 @@ describe "generate_range" do
   it "start point 1, any step" do
     expect(generate_range(1,10,2)).to eq [1,3,5,7,9]
   end
+  it "any start point, any step" do
+    expect(generate_range(25,50,5)).to eq [25, 30, 35, 40, 45, 50]
+  end
 end
