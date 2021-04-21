bcb5a59ac0524f786cc8f295a7ddf75ef044ccd5
xavierloos <xavier.loos04@gmail.com>
2021-04-21 13:48:51 +0100

    return an array

--- JS/spec/GenerateRangeIntSpec.js
+++ JS/spec/GenerateRangeIntSpec.js
@@ -3,4 +3,8 @@ describe("GenerateRangeInt", () => {
   it("should generate an array", () => {
     expect(generateRange(2, 10, 2)).toEqual([2,4,6,8,10]);
   });
+
+  it("should return an array", () => {
+    expect(generateRange(2, 10, 2)).toBeInstanceOf(Array);
+  });
 });
