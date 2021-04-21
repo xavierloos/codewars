379e848ec6d1400fca2ee16c3876d6e9f6c5f259
xavierloos <xavier.loos04@gmail.com>
2021-04-21 13:20:35 +0100

    Intro

--- JS/src/GenerateRangeInt.js
+++ JS/src/GenerateRangeInt.js
@@ -1,1 +1,3 @@
 // https://www.codewars.com/kata/55eca815d0d20962e1000106/train/javascript
+
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
