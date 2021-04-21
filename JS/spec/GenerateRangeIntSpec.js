a9c672076dffe12b8339d916f615cb5a3dbc54ad
xavierloos <xavier.loos04@gmail.com>
2021-04-21 14:23:25 +0100

    Contains only numbers

--- JS/spec/GenerateRangeIntSpec.js
+++ JS/spec/GenerateRangeIntSpec.js
@@ -7,4 +7,11 @@ describe("GenerateRangeInt", () => {
   it("should return an array", () => {
     expect(generateRange(2, 10, 2)).toBeInstanceOf(Array);
   });
+
+  it("contains only numbers", () => {
+    var a = generateRange(2, 10, 2)
+    expect(a[0]).not.toBeInstanceOf(String);
+    console.log()
+    expect(a[a.length]).not.toBeInstanceOf(String);
+  });
 });
