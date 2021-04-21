6fe7789e9d3005b8637533de1502d7a4437d4ff6
xavierloos <xavier.loos04@gmail.com>
2021-04-21 13:21:30 +0100

    Note

--- JS/src/GenerateRangeInt.js
+++ JS/src/GenerateRangeInt.js
@@ -7,3 +7,8 @@
 
 // generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
 // generateRange(1, 10, 3) // should return array of [1,4,7,10]
+
+// Note
+// min < max
+// step > 0
+// the range does not HAVE to include max (depending on the step)
