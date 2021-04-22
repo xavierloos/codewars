9f547c07402843c8c382457ee88c9f334fff43e5
xavierloos <xavier.loos04@gmail.com>
2021-04-22 21:30:07 +0100

    Should return a type of string

--- JS/spec/AlternateSpec.js
+++ JS/spec/AlternateSpec.js
@@ -2,4 +2,7 @@ describe("alternateCase", () => {
   it("should alternate the string", () => {
     expect(alternateCase("abc")).toEqual("ABC");
   });
+  it("should return a string", () => {
+    expect(alternateCase("abc")).toBeInstanceOf(String);
+  });
 });
