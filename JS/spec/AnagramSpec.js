fc325616aab572fbef2f323aec11cc27f280cc56
xavierloos <xavier.loos04@gmail.com>
2021-04-23 12:51:00 +0100

    Return true

--- JS/spec/AnagramSpec.js
+++ JS/spec/AnagramSpec.js
@@ -1,5 +1,5 @@
 describe("_isAnagram", () => {
   it("returns true if contains the same letters", () => {
-    expect(_isAnagram("Debit card", "Bad credit")).toEqual("true")
+    expect(_isAnagram("Debit card", "Bad credit")).toEqual(true)
   });
 })
--- JS/src/Anagram.js
+++ JS/src/Anagram.js
@@ -1,4 +1,5 @@
 // Check is the 2 strings passed are anagrams
 function _isAnagram(str1, str2) {
   // Your code here
+  return true
 }
