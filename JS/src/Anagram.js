c5dcce03ea2bcea00dc400bd653062cf612d4c25
xavierloos <xavier.loos04@gmail.com>
2021-04-23 13:05:28 +0100

    To lower case

--- JS/src/Anagram.js
+++ JS/src/Anagram.js
@@ -2,6 +2,6 @@
 function _isAnagram(str1, str2) {
   // Your code here
   counter = 0
-  str1 = str1.split(" ").join("")
-  str2 = str2.split(" ").join("")
+  str1 = str1.toLowerCase().split(" ").join("")
+  str2 = str1.toLowerCase().split(" ").join("")
 }
