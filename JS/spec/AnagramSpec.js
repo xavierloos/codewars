5ea7374631eae2bd4e17b507e1ba339c871a284e
xavierloos <xavier.loos04@gmail.com>
2021-04-23 12:54:43 +0100

    Return false if strings are differents letters

--- JS/spec/AnagramSpec.js
+++ JS/spec/AnagramSpec.js
@@ -2,4 +2,7 @@ describe("_isAnagram", () => {
   it("returns true if contains the same letters", () => {
     expect(_isAnagram("Debit card", "Bad credit")).toEqual(true)
   });
+  it("return false if contains differents letter", () => {
+    expect(_isAnagram("Debit card", "Bacredit")).toEqual(false)
+  })
 })
