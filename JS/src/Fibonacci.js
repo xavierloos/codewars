a7e3b3d1a774e5bce74389536c959119533070ce
xavierloos <xavier.loos04@gmail.com>
2021-04-24 11:54:22 +0100

    Intro

---
+++ JS/src/Fibonacci.js
@@ -0,0 +1,3 @@
+// In mathematics, the Fibonacci numbers, commonly denoted Fn, form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,[1]
+
+// OUTPUT = [ 0,   1,   1,  2,  3,  5, 8,  13,  21, 34, 55, 89, 144, 233, 377]
