01b0309844e0d46dc06f01d57a2280c1406b7f68
xavierloos <xavier.loos04@gmail.com>
2021-04-25 19:26:27 +0100

    It escaped

--- JS/spec/CatVsMouseSpec.js
+++ JS/spec/CatVsMouseSpec.js
@@ -8,4 +8,7 @@ describe("catMouse", () => {
   it("should be caught!", () => {
     expect(catMouse('C..m')).toEqual("Caught!")
   })
+  it("should escaped", () => {
+    expect(catMouse('C.....m')).toEqual("Escaped!")
+  })
 })
