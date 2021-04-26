7485f8a093bbc4fec870b83db966bda451aca84f
xavierloos <xavier.loos04@gmail.com>
2021-04-26 20:44:01 +0100

    Returns 1

--- JS/spec/MiddleElementSpec.js
+++ JS/spec/MiddleElementSpec.js
@@ -2,5 +2,7 @@ describe("MiddleElement", () => {
     it("shoud find the index of middle element", () => {
       expect(gimme([2, 3, 1])).toEqual(0);
     });
-
+    it("shoud find the index of middle element", () => {
+      expect(gimme([5, 7, 9])).toEqual(1);
+    });
 });
