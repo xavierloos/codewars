2975edc4a49e40c41bb13411a411ef1e39a748a1
xavierloos <xavier.loos04@gmail.com>
2021-04-26 20:33:21 +0100

    Adding function

--- JS/src/MiddleElement.js
+++ JS/src/MiddleElement.js
@@ -12,3 +12,8 @@
 
 // gimme([5, 10, 14]) => 1
 // 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+var gimme = function (inputArray) {
+  // Implement this function
+
+};
