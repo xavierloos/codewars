3699e238f1a93d42fad84699eb62c937ad2a5b87
xavierloos <xavier.loos04@gmail.com>
2021-04-27 21:05:24 +0100

    Examples

--- JS/src/FindVowels.js
+++ JS/src/FindVowels.js
@@ -3,3 +3,9 @@
 // We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
 
 // So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
