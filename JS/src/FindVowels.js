7a017b7ba9c61cbf28d600b01e6c40bdd5f3b12e
xavierloos <xavier.loos04@gmail.com>
2021-04-27 21:04:41 +0100

    Intro

--- JS/src/FindVowels.js
+++ JS/src/FindVowels.js
@@ -1,1 +1,5 @@
 // https://www.codewars.com/kata/5680781b6b7c2be860000036/train/javascript
+
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
