a6fb19a5783bdfd89a1a2d6e1ca9d09b4a75bb00
xavierloos <xavier.loos04@gmail.com>
2021-04-27 21:22:09 +0100

    Returns empty

--- JS/spec/FindVowelsSpec.js
+++ JS/spec/FindVowelsSpec.js
@@ -2,4 +2,7 @@ describe("vowelIndices", () => {
   it("should return a type of array", () => {
     expect(vowelIndices("array")).toBeInstanceOf(Array)
   })
+  it("should return empty", () => {
+    expect(vowelIndices("mmmm")).toEqual([])
+  })
 })
