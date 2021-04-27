b06b1acffe1a4fc2e8189f940b414fdef3ac4dfe
xavierloos <xavier.loos04@gmail.com>
2021-04-27 21:05:50 +0100

    Notes

--- JS/src/FindVowels.js
+++ JS/src/FindVowels.js
@@ -9,3 +9,7 @@
 // Super => [2,4]
 // Apple => [1,5]
 // YoMama -> [1,2,4,6]
+
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
