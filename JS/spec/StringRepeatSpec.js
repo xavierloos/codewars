aba4d920a939f7947641a9ff8ec2d3e5661f01ac
xavierloos <xavier.loos04@gmail.com>
2021-04-29 20:16:23 +0100

    Should return ***

--- JS/spec/StringRepeatSpec.js
+++ JS/spec/StringRepeatSpec.js
@@ -2,4 +2,7 @@ describe("repeatStr", () => {
   it("should return a type of string", () => {
     expect(repeatStr(3, "*")).toBeInstanceOf(String)
   })
+  it("should return ***", () => {
+    expect(repeatStr(3, "*")).toEqual("***")
+  })
 })
