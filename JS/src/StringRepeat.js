c019d362f5614945b86205dc6b1be1ed8174b192
xavierloos <xavier.loos04@gmail.com>
2021-04-29 20:04:45 +0100

    Intro

--- JS/src/StringRepeat.js
+++ JS/src/StringRepeat.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
