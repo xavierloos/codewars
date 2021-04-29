8b919d032b3c6d4a135f26b0070b47a3eb4f2bba
xavierloos <xavier.loos04@gmail.com>
2021-04-29 17:17:15 +0100

    Should return 94

--- JS/spec/AverageScoreSpec.js
+++ JS/spec/AverageScoreSpec.js
@@ -5,4 +5,7 @@ describe("average", () => {
   it("shoul return 73", () => {
     expect(average([49,3,5,300,7])).toEqual(73)
   })
+  it("shoul return 94", () => {
+    expect(average([90,98,89,100,100,86,94])).toEqual(94)
+  })
 })
