6c4ee5cb159494622f9b9167d3c16539f049c145
xavierloos <xavier.loos04@gmail.com>
2021-04-30 16:35:01 +0100

    update

--- JS/spec/GenerateRangeIntSpec.js
+++ JS/spec/GenerateRangeIntSpec.js
@@ -3,11 +3,9 @@ describe("GenerateRangeInt", () => {
   it("should generate an array", () => {
     expect(generateRange(2, 10, 2)).toEqual([2,4,6,8,10]);
   });
-
   it("should return an array", () => {
     expect(generateRange(2, 10, 2)).toBeInstanceOf(Array);
   });
-
   it("contains only numbers", () => {
     var a = generateRange(2, 10, 2)
     expect(a[0]).not.toBeInstanceOf(String);
