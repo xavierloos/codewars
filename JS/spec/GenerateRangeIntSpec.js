3b7aaa9d3139b86e9bcbcdaac815fe02fccb483f
xavierloos <xavier.loos04@gmail.com>
2021-04-30 16:37:48 +0100

    Refactor

--- JS/spec/GenerateRangeIntSpec.js
+++ JS/spec/GenerateRangeIntSpec.js
@@ -3,7 +3,6 @@ describe("GenerateRangeInt", () => {
   it("should generate an array", () => {
     expect(generateRange(2, 10, 2)).toEqual([2,4,6,8,10]);
   });
-  
   it("should return an array", () => {
     expect(generateRange(2, 10, 2)).toBeInstanceOf(Array);
   });
