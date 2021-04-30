b1b38ba211b4897d26b169e1493faf5c8532af36
xavierloos <xavier.loos04@gmail.com>
2021-04-30 16:47:26 +0100

    Refactor

--- Ruby/lib/credit_card_mask.rb
+++ Ruby/lib/credit_card_mask.rb
@@ -9,7 +9,6 @@
 # maskify('64607935616')      # should return '#######5616'
 # maskify('1')                # should return '1'
 # maskify('')                 # should return ''
-
 # # "What was the name of your first pet?"
 # maskify('Skippy')                                   # should return '##ippy'
 # maskify('Nananananananananananananananana Batman!') # should return '####################################man!'
