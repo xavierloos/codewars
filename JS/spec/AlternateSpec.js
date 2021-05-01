54cd78b51e9a87daeec33c040d1b667176bce820
xavierloos <xavier.loos04@gmail.com>
2021-05-01 11:15:57 +0100

    Refactor

--- JS/spec/AlternateSpec.js
+++ JS/spec/AlternateSpec.js
@@ -2,10 +2,11 @@ describe("alternateCase", () => {
   it("should alternate the string", () => {
     expect(alternateCase("abc")).toEqual("ABC");
   });
-  
+
   it("should return a string", () => {
     expect(alternateCase("abc")).toBeInstanceOf(String);
   });
+  
   it("should return AbC", () => {
     expect(alternateCase("aBc")).toEqual("AbC");
   });
