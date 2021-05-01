8e3eb45c0516822dfc324ca586f0d03d90c87144
xavierloos <xavier.loos04@gmail.com>
2021-05-01 11:23:10 +0100

    Space

--- JS/spec/FindVowelsSpec.js
+++ JS/spec/FindVowelsSpec.js
@@ -2,10 +2,11 @@ describe("vowelIndices", () => {
   it("should return a type of array", () => {
     expect(vowelIndices("array")).toBeInstanceOf(Array)
   })
-  
+
   it("should return empty", () => {
     expect(vowelIndices("mmmm")).toEqual([])
   })
+  
   it("should return 2 and 5", () => {
     expect(vowelIndices("hello")).toEqual([2,5])
   })
