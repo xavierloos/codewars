cb70e7bd62443d2c53e80022888fd91daf5103a8
xavierloos <xavier.loos04@gmail.com>
2021-05-01 11:21:26 +0100

    Space

--- JS/spec/CatVsMouseSpec.js
+++ JS/spec/CatVsMouseSpec.js
@@ -2,10 +2,11 @@ describe("catMouse", () => {
   it("Shoudl scape", () => {
     expect(catMouse('C....m')).toEqual("Escaped!")
   })
-  
+
   it("should return a string", () => {
     expect(catMouse('C....m')).toBeInstanceOf(String)
   })
+  
   it("should be caught!", () => {
     expect(catMouse('C..m')).toEqual("Caught!")
   })
