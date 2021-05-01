b65da70d517aed03b64e85ee615c3389e6b8be46
xavierloos <xavier.loos04@gmail.com>
2021-05-01 11:23:19 +0100

    Space

--- JS/spec/FindVowelsSpec.js
+++ JS/spec/FindVowelsSpec.js
@@ -6,10 +6,11 @@ describe("vowelIndices", () => {
   it("should return empty", () => {
     expect(vowelIndices("mmmm")).toEqual([])
   })
-  
+
   it("should return 2 and 5", () => {
     expect(vowelIndices("hello")).toEqual([2,5])
   })
+  
   it("should return works with capitals letters", () => {
     expect(vowelIndices("hEllO")).toEqual([2,5])
   })
