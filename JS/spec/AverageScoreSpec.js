8b277e0d429225e7481472967e9f568a9771fdae
xavierloos <xavier.loos04@gmail.com>
2021-05-01 11:19:51 +0100

    Space

--- JS/spec/AverageScoreSpec.js
+++ JS/spec/AverageScoreSpec.js
@@ -2,10 +2,11 @@ describe("average", () => {
   it("shoul return an integer", () => {
     expect(average([49,3,5,300,7])).toBeInstanceOf(Number)
   })
-  
+
   it("shoul return 73", () => {
     expect(average([49,3,5,300,7])).toEqual(73)
   })
+  
   it("shoul return 94", () => {
     expect(average([90,98,89,100,100,86,94])).toEqual(94)
   })
