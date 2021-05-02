8cf43904c7cb5d8617ae1634b64a8890e8203f71
xavierloos <xavier.loos04@gmail.com>
2021-05-02 11:02:41 +0100

    Should return equal 8

--- JS/spec/SmallestInArraySpec.js
+++ JS/spec/SmallestInArraySpec.js
@@ -2,5 +2,8 @@ describe("SmallestIntegerFinder",()=> {
   it("should return a number", () => {
     expect(findSmallestInt([78,56,232,12,8])).toBeInstanceOf(Number)
   })
+  it("Should return the smallest int 8", () => {
+    expect(findSmallestInt([78,56,232,12,8])).toEqual(8)
+  })
 })
 
