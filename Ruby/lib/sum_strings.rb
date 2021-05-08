147ca7c474e4aa4c26f228cdc13e58bc8e41b11f
xavierloos <xavier.loos04@gmail.com>
2021-05-08 16:19:27 +0100

    Intro

--- Ruby/lib/sum_strings.rb
+++ Ruby/lib/sum_strings.rb
@@ -1,1 +1,3 @@
 # codewars.com/kata/5966e33c4e686b508700002d/train/ruby
+
+# Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
