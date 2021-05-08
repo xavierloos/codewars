5b0f3a3c13704535f40b66678c73b1832ffdcecf
xavierloos <xavier.loos04@gmail.com>
2021-05-08 16:31:29 +0100

    Returns 39

--- Ruby/spec/sum_strings_spec.rb
+++ Ruby/spec/sum_strings_spec.rb
@@ -4,7 +4,10 @@ describe "sum_str" do
   it "returns 9" do
     expect(sum_str("4","5")).to eq  "9"
   end
-  it "returns 9" do
+  it "returns a string" do
     expect(sum_str("4","5")).to be_a_kind_of(String) 
+  end
+  it "returns 39" do
+    expect(sum_str("34","5")).to eq  "39"
   end
 end
