59a347f3056e75cc0579a0f948d562618d6092fe
xavierloos <xavier.loos04@gmail.com>
2021-05-09 21:04:14 +0100

    Refactor

--- Ruby/lib/sum_strings.rb
+++ Ruby/lib/sum_strings.rb
@@ -1,5 +1,4 @@
 # codewars.com/kata/5966e33c4e686b508700002d/train/ruby
-
 # Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
 # Example: (Input1, Input2 -->Output)
 # "4",  "5" --> "9"
