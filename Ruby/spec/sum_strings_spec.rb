7031eb6a852eab3b06eb8addb95e6a4b432e279b
xavierloos <xavier.loos04@gmail.com>
2021-05-09 21:02:32 +0100

    Deleting space

--- Ruby/spec/sum_strings_spec.rb
+++ Ruby/spec/sum_strings_spec.rb
@@ -7,7 +7,6 @@ describe "sum_str" do
   it "returns a string" do
     expect(sum_str("4","5")).to be_a_kind_of(String) 
   end
-
   it "returns 39" do
     expect(sum_str("34","5")).to eq  "39"
   end
