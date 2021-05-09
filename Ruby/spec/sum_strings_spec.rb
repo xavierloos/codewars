e43afef4c34619e18af84cc254c54f893b68ff4b
xavierloos <xavier.loos04@gmail.com>
2021-05-09 21:00:29 +0100

    Update

--- Ruby/spec/sum_strings_spec.rb
+++ Ruby/spec/sum_strings_spec.rb
@@ -1,10 +1,11 @@
 require "sum_strings"
 
 describe "sum_str" do
+  
   it "returns 9" do
     expect(sum_str("4","5")).to eq  "9"
   end
-  
+
   it "returns a string" do
     expect(sum_str("4","5")).to be_a_kind_of(String) 
   end
