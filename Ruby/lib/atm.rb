9328f437dd87413d199f919b50acb06e30cae446
xavierloos <xavier.loos04@gmail.com>
2021-05-14 13:41:57 +0100

    Example

--- Ruby/lib/atm.rb
+++ Ruby/lib/atm.rb
@@ -1,1 +1,6 @@
 # Buil a ATM machine that dispenses the correct denomination to the user
+
+#Example
+# If you request 110 dollars the second argument will ask for all the denomination the user wants and this will return a hash that has the denomitaions as the key and the value is the number of that denomination
+# INPUT                      OUTPUT
+# ATM(100,[100,20,10,5]) => {100 =>1,20=>0,10=>1,5=>0}
