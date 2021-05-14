3f24ba95b9ced13ec359270ada7c965df3f25f97
xavierloos <xavier.loos04@gmail.com>
2021-05-14 13:43:54 +0100

    Def

--- Ruby/lib/atm.rb
+++ Ruby/lib/atm.rb
@@ -4,3 +4,7 @@
 # If you request 110 dollars the second argument will ask for all the denomination the user wants and this will return a hash that has the denomitaions as the key and the value is the number of that denomination
 # INPUT                      OUTPUT
 # ATM(100,[100,20,10,5]) => {100 =>1,20=>0,10=>1,5=>0}
+
+def atm total, denomitaions
+  # Your code here
+end
