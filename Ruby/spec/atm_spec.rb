25fd02781c0df6c382d4cfaf0b1dc53439ac90ee
xavierloos <xavier.loos04@gmail.com>
2021-05-14 14:07:52 +0100

    Give change of 520

--- Ruby/spec/atm_spec.rb
+++ Ruby/spec/atm_spec.rb
@@ -4,4 +4,7 @@ describe "atm" do
   it "dispenses the correct denominations to users" do
     expect(atm 110, [100,20,10,5]).to eq({100 =>1,20=>0,10=>1,5=>0})
   end
+  it "dispenses the correct denominations to users" do
+    expect(atm 520, [100,20,10,5]).to eq({100 =>5,20=>1,10=>0,5=>0})
+  end
 end
