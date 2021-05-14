c38caff8fcc6d9ddd54382b1fa7032de88206e7b
xavierloos <xavier.loos04@gmail.com>
2021-05-14 14:06:49 +0100

    Test passed

--- Ruby/spec/atm_spec.rb
+++ Ruby/spec/atm_spec.rb
@@ -2,6 +2,6 @@ require "atm"
 
 describe "atm" do
   it "dispenses the correct denominations to users" do
-    expect(atm 110, [100,20,10,5]).toeq ({100 =>1,20=>0,10=>1,5=>0})
+    expect(atm 110, [100,20,10,5]).to eq({100 =>1,20=>0,10=>1,5=>0})
   end
 end
