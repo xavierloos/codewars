4149842777bd52eca7b655a80d9f59dc9be9c9c7
xavierloos <xavier.loos04@gmail.com>
2021-05-15 13:39:36 +0100

    Example

--- Ruby/lib/hash_fizzbuzz.rb
+++ Ruby/lib/hash_fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Create the fizzbuzz example but with a little bit of twist
 # The function should return a hash with the number as a key and the result as a value.
+
+# Example
+# {
+#   1=>1,
+#   2=>2,
+#   3=>Fizz,
+#   4=>4,
+#   5=>Buzz,
+#   etc.
+# }
