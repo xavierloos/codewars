d04ebe63877f612795047fe6eaffe3201a790e0d
xavierloos <xavier.loos04@gmail.com>
2021-05-15 13:47:08 +0100

    Raise an error on lower than 1

--- Ruby/spec/hash_fizzbuzz_spec.rb
+++ Ruby/spec/hash_fizzbuzz_spec.rb
@@ -4,5 +4,9 @@ describe "fizzbuzz" do
   it "should return a type of hash" do
     expect(fizzbuzz 1).to be_a_kind_of(Hash)
   end
+  it "should raise an error if the argument is lower than 1" do
+    expect {fizzbuzz 0 } .to raise_error "Input invalid"
+  end
+
   
 end
