406101b3921503fa4d5f03d0d78c39c058802946
xavierloos <xavier.loos04@gmail.com>
2021-05-15 13:51:58 +0100

    Says Fizz

--- Ruby/spec/hash_fizzbuzz_spec.rb
+++ Ruby/spec/hash_fizzbuzz_spec.rb
@@ -7,6 +7,14 @@ describe "fizzbuzz" do
   it "should raise an error if the argument is lower than 1" do
     expect {fizzbuzz 0 } .to raise_error "Input invalid"
   end
+  it "should says fizz on the number 3" do
+    final_hash = {
+      1=>1,
+      2=>2,
+      3=>"fizz"
+    }
+    expect(fizzbuzz 3).to eq final_hash
+  end
 
   
 end
