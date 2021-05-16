f5f0497c9089898a519a054a1285eb2cd5d0bacf
xavierloos <xavier.loos04@gmail.com>
2021-05-16 10:38:29 +0100

    Break

--- Ruby/spec/hash_fizzbuzz_spec.rb
+++ Ruby/spec/hash_fizzbuzz_spec.rb
@@ -4,10 +4,11 @@ describe "fizzbuzz" do
   it "should return a type of hash" do
     expect(fizzbuzz 1).to be_a_kind_of(Hash)
   end
+  
   it "should raise an error if the argument is lower than 1" do
     expect {fizzbuzz 0 } .to raise_error "Input invalid"
   end
-  
+
   it "should say fizz on the number 3" do
     final_hash = {
       1=>1,
