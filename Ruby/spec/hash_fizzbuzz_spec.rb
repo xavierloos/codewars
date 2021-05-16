d665b93ff8769efa2a6948790690ce9287953560
xavierloos <xavier.loos04@gmail.com>
2021-05-16 10:38:40 +0100

    Update

--- Ruby/spec/hash_fizzbuzz_spec.rb
+++ Ruby/spec/hash_fizzbuzz_spec.rb
@@ -1,10 +1,11 @@
 require "hash_fizzbuzz"
 
 describe "fizzbuzz" do
+  
   it "should return a type of hash" do
     expect(fizzbuzz 1).to be_a_kind_of(Hash)
   end
-  
+
   it "should raise an error if the argument is lower than 1" do
     expect {fizzbuzz 0 } .to raise_error "Input invalid"
   end
