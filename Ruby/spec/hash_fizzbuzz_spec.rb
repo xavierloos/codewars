158045497ade684bfa857c052f43b5b209f27734
xavierloos <xavier.loos04@gmail.com>
2021-05-16 10:38:54 +0100

    Update

--- Ruby/spec/hash_fizzbuzz_spec.rb
+++ Ruby/spec/hash_fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require "hash_fizzbuzz"
 
 describe "fizzbuzz" do
-  
+
   it "should return a type of hash" do
     expect(fizzbuzz 1).to be_a_kind_of(Hash)
   end
@@ -26,6 +26,7 @@ describe "fizzbuzz" do
       3=>"fizz",
       4=>4,
       5=>"buzz"
+      
     }
     expect(fizzbuzz 5).to eq final_hash
   end
