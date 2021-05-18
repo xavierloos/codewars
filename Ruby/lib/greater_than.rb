d998ae0a34259c6ef42b96ff351e15e679b9b906
xavierloos <xavier.loos04@gmail.com>
2021-05-18 16:21:40 +0100

    Example

--- Ruby/lib/greater_than.rb
+++ Ruby/lib/greater_than.rb
@@ -1,2 +1,5 @@
 # Create a function that get 2 arguments (the number you want to check, and the list of numbers you want to evaluate.) to see how many times is greater than the numbers passed.
 
+# Example
+# Input                   Output
+# 5, [5,6,7,1,2] => 2 (5 is not greater than 5 so it does not count)
