5c610dad2007065dafb2bce584fd241cee247b54
xavierloos <xavier.loos04@gmail.com>
2021-05-18 16:42:39 +0100

    Test: Valid second argument

--- Ruby/spec/greater_than_spec.rb
+++ Ruby/spec/greater_than_spec.rb
@@ -5,9 +5,13 @@ describe "greater_than" do
     expect(greater_than 5, [4,5,6,7]).to be_a_kind_of(Numeric)  
   end
 
-  it "should raise an error if the fisrt argument is not a valid number or negative" do
+  it "should raise an error if the first argument is not a valid number or negative" do
     expect{greater_than "5", [4,5,6,7]}.to raise_error "Invalid number or negative"
     expect{greater_than 0, [4,5,6,7]}.to raise_error "Invalid number or negative"
   end
   
+  it "should raise an error if the sec argument is not an array or empy" do
+    expect{greater_than "5", []}.to raise_error "Invalid argument or empty"
+    e
+  end
 end
