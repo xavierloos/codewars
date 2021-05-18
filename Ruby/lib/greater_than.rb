08ff2fc21f3f448ea3dcc7b7d34395bdf3d5b058
xavierloos <xavier.loos04@gmail.com>
2021-05-18 16:20:07 +0100

    Intro

--- Ruby/lib/greater_than.rb
+++ Ruby/lib/greater_than.rb
@@ -1,1 +1,2 @@
-# Create a function that get 2 arguments (the number you want to check, and the list of numbers you want to evaluate.) to see if it is greater than the numbers passed.
+# Create a function that get 2 arguments (the number you want to check, and the list of numbers you want to evaluate.) to see how many times is greater than the numbers passed.
+
