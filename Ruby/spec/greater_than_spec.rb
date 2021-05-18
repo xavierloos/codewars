709b85ef0530bbe83785450a2359f196e21d4068
xavierloos <xavier.loos04@gmail.com>
2021-05-18 16:32:25 +0100

    Test: Raise an error if negative of invalid number

--- Ruby/spec/greater_than_spec.rb
+++ Ruby/spec/greater_than_spec.rb
@@ -4,5 +4,9 @@ describe "greater_than" do
   it "should return a type of number" do
     expect(greater_than 5, [4,5,6,7]).to be_a_kind_of(Numeric)  
   end
+
+  it "should raise an error if the fisrt argument is not a valid number or negative" do
+    expect{greater_than "5", [4,5,6,7]}.to raise_error "Invalid number or negative"
+  end
   
 end
