13003c208bdea6af936ed5134fa006239d7f0075
xavierloos <xavier.loos04@gmail.com>
2021-05-19 20:07:46 +0100

    return the same method is_prime if eq to 0

--- Ruby/lib/prime.rb
+++ Ruby/lib/prime.rb
@@ -4,15 +4,12 @@ def count_primes array
   # You code here
   raise "List of number invalid" if !array.is_a? Array
   array.count do |item|
-    is_prime(item, number - 1)
+    is_prime(item, item - 1)
   end
 end
 
 def is_prime(item,number)
   return false if item == 1
   return true if number == 1
-  (2..(item-1)).each do |number|
-    return false if item % number == 0
-  end
-  return true
+  return is_prime(item,number-1) if item % number != 0
 end
