a8e7d2adc3f5fc54bff0636f3e7c9742e501d927
xavierloos <xavier.loos04@gmail.com>
2021-05-19 13:16:43 +0100

    Refactor

--- Ruby/lib/prime.rb
+++ Ruby/lib/prime.rb
@@ -11,9 +11,7 @@ end
 def is_prime item
   return false if item == 1
   (2..(item-1)).each do |number|
-    if item % number == 0
-      return false
-    end
+    return false if item % number == 0
   end
   return true
 end
