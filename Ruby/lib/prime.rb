d3b474a33878f337ead9790ce1e35f2b6549f7d3
xavierloos <xavier.loos04@gmail.com>
2021-05-19 12:23:28 +0100

    Ugly code with test

--- Ruby/lib/prime.rb
+++ Ruby/lib/prime.rb
@@ -2,4 +2,23 @@
 
 def count_primes array
   # You code here
+  raise "List of number invalid" if !array.is_a? Array
+  total = 0
+  for item in array
+    next if item == 1
+    is_prime = true
+    number = item - 1
+    while number > 1
+      if item % number == 0
+        is_prime=false
+        break
+      else
+        number = number - 1
+      end
+    end
+    if is_prime == true
+      total = total+1
+    end
+  end
+  total
 end
---
+++ Ruby/spec/prime_spec.rb
@@ -0,0 +1,14 @@
+require "prime"
+
+describe "count_primes" do
+  it "returns a type of number" do
+    expect(count_primes [1,2,3,4,5,6,7,8,9,10]).to be_a_kind_of(Numeric)
+  end
+  it "raises an error if the arguments is not an array" do
+    expect{count_primes "1,2,3,5"}.to raise_error "List of number invalid"
+    expect{count_primes 1}.to raise_error "List of number invalid"
+  end
+  it "return the number 4 wich are the primes in the array" do
+    expect(count_primes [1,2,3,4,5,6,7,8,9,10]).to eq 4
+  end
+end
