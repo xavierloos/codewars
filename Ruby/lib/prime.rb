8cd2d7ce8bb4ae7fd2f318f5b5e4742a20225cd1
xavierloos <xavier.loos04@gmail.com>
2021-05-19 12:27:48 +0100

    Split a long method

--- Ruby/lib/prime.rb
+++ Ruby/lib/prime.rb
@@ -5,20 +5,24 @@ def count_primes array
   raise "List of number invalid" if !array.is_a? Array
   total = 0
   for item in array
-    next if item == 1
-    is_prime = true
-    number = item - 1
-    while number > 1
-      if item % number == 0
-        is_prime=false
-        break
-      else
-        number = number - 1
-      end
-    end
+    is_prime = is_prime(item)
     if is_prime == true
       total = total+1
     end
   end
   total
+end
+
+def is_prime item
+  return false if item == 1
+  number = item - 1
+  while number > 1
+    if item % number == 0
+      return false
+      break
+    else
+      number = number - 1
+    end
+  end
+  return true
 end
