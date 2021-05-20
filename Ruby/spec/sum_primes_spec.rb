95b928771020bd72852fcfa8ba18dbbbe050339b
xavierloos <xavier.loos04@gmail.com>
2021-05-20 13:19:42 +0100

    Raises error if the arguments is not an array

--- Ruby/spec/sum_primes_spec.rb
+++ Ruby/spec/sum_primes_spec.rb
@@ -4,5 +4,8 @@ describe "sum_primes" do
   it "returns a type of number" do
     expect(sum_primes [1,2,3,4,5,6,7,8,9,10]).to be_a_kind_of(Numeric)
   end
+  it "raises an error if the arg is not an array" do
+    expect{sum_primes "1,2,3,4"}.to raise_error "Argument should be an array"
+  end
   
 end
