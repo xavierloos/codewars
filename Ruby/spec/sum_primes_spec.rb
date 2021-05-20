6b0d0410026858ce08f0169977c359f2f544792a
xavierloos <xavier.loos04@gmail.com>
2021-05-20 13:30:44 +0100

    Returns the sum of the array

--- Ruby/spec/sum_primes_spec.rb
+++ Ruby/spec/sum_primes_spec.rb
@@ -7,5 +7,7 @@ describe "sum_primes" do
   it "raises an error if the arg is not an array" do
     expect{sum_primes "1,2,3,4"}.to raise_error "Argument should be an array"
   end
-  
+  it "returns the sum of the array" do
+    expect(sum_primes [1,2,3,4,5,6,7,8,9,10]).to eq 17
+  end
 end
