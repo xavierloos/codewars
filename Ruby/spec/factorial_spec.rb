3e1b8103d4e56af388bee9a9c7c3a3787679759e
xavierloos <xavier.loos04@gmail.com>
2021-05-20 13:55:20 +0100

    Returns 120

--- Ruby/spec/factorial_spec.rb
+++ Ruby/spec/factorial_spec.rb
@@ -7,4 +7,7 @@ describe "factorial" do
   it "receives a number as an argument" do
     expect{factorial "5"}.to raise_error "Wrong type of argument"
   end
+  it "returns 120 which is the factorial of 5" do
+    expect(factorial 5).to eq 120
+  end
 end
