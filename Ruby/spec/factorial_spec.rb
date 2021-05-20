c45a517719c24e3ec586d2434d8097e9372159db
xavierloos <xavier.loos04@gmail.com>
2021-05-20 13:53:31 +0100

    Raise an error on wrong type of argument

--- Ruby/spec/factorial_spec.rb
+++ Ruby/spec/factorial_spec.rb
@@ -4,5 +4,7 @@ describe "factorial" do
   it "returns a type of numeric" do
     expect(factorial 5).to be_a_kind_of(Numeric)
   end
-  
+  it "receives a number as an argument" do
+    expect{factorial "5"}.to raise_error "Wrong type of argument"
+  end
 end
