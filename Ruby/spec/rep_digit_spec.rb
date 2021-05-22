670ab546c035a033025a64928ced1a90174e8d06
xavierloos <xavier.loos04@gmail.com>
2021-05-22 10:10:34 +0100

    Should raise an error

--- Ruby/spec/rep_digit_spec.rb
+++ Ruby/spec/rep_digit_spec.rb
@@ -12,4 +12,7 @@ describe "repeated_digit" do
   it "should pass a Boolean" do
     expect(repeated_digit 14).to be_a_kind_ok(boolean)
   end
+  it "should raise an error if the argument is not a number" do
+    expect{repeated_digit "14"}.to raise_error "Wrong type of argument"
+  end
 end
