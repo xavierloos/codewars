0cd93723eb03bf9f9124a3b9765c8c472c26813f
xavierloos <xavier.loos04@gmail.com>
2021-05-22 10:08:36 +0100

    Space

--- Ruby/spec/rep_digit_spec.rb
+++ Ruby/spec/rep_digit_spec.rb
@@ -4,10 +4,11 @@ describe "repeated_digit" do
   it "should return true" do
     expect(repeated_digit 14).to eq true
   end
-  
+
   it "should return false" do
     expect(repeated_digit 14).to eq false
   end
+  
   it "should pass a Boolean" do
     expect(repeated_digit 14).to be_a_kind_ok(Boolean)
   end
