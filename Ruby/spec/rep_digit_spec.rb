75a2d84fc2c1ea29cf886bd3b8fb1422a53cc98f
xavierloos <xavier.loos04@gmail.com>
2021-05-23 20:50:42 +0100

    Update

--- Ruby/spec/rep_digit_spec.rb
+++ Ruby/spec/rep_digit_spec.rb
@@ -7,10 +7,11 @@ describe "repeated_digit" do
   it "should return false" do
     expect(repeated_digit 14).to eq false
   end
+  
   it "should pass a Boolean" do
     expect(repeated_digit 14).to be_a_kind_ok(boolean)
   end
-  
+
   it "should raise an error if the argument is not a number" do
     expect{repeated_digit "14"}.to raise_error "Wrong type of argument"
   end
