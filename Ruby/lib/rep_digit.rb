02a798aacaa19b1252c5dfcf962f9c65179d1817
xavierloos <xavier.loos04@gmail.com>
2021-05-24 21:25:56 +0100

    It's true

--- Ruby/lib/rep_digit.rb
+++ Ruby/lib/rep_digit.rb
@@ -6,7 +6,7 @@
 def repeated_digit number
   array = number.to_s.split("").map(&:to_i)
   digit = array.first
-  count = array.count{ |number| number != digits}
+  count = array.count{ |number| number != digit}
   if count == 0
     return true
   else
--- Ruby/spec/rep_digit_spec.rb
+++ Ruby/spec/rep_digit_spec.rb
@@ -2,7 +2,7 @@ require "rep_digit"
 
 describe "repeated_digit" do
   it "should return true" do
-    expect(repeated_digit 14).to eq true
+    expect(repeated_digit 777).to eq true
   end
 
   it "should return false" do
@@ -12,7 +12,7 @@ describe "repeated_digit" do
   it "should pass a Boolean" do
     expect(repeated_digit 14).to be_a_kind_ok(boolean)
   end
-  
+
   it "should raise an error if the argument is not a number" do
     expect{repeated_digit "14"}.to raise_error "Wrong type of argument"
   end
