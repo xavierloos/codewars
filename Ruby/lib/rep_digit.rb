f78104773777eead39c28b103fc3f2cb2f0e91be
xavierloos <xavier.loos04@gmail.com>
2021-05-24 21:26:12 +0100

    Raise error

--- Ruby/lib/rep_digit.rb
+++ Ruby/lib/rep_digit.rb
@@ -4,6 +4,7 @@
 # 445 => [4,4,5] => false
 
 def repeated_digit number
+  raise "Wrong type of argument" if !number.is_a? Numeric
   array = number.to_s.split("").map(&:to_i)
   digit = array.first
   count = array.count{ |number| number != digit}
