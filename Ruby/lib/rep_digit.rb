d9fdad13d651f1a02444bc239fdbac39b33a4e0d
xavierloos <xavier.loos04@gmail.com>
2021-05-25 12:32:09 +0100

    Using SQUEEZE method

--- Ruby/lib/rep_digit.rb
+++ Ruby/lib/rep_digit.rb
@@ -7,5 +7,5 @@ def repeated_digit number
   raise "Wrong type of argument" if !number.is_a? Numeric
   # array = number.to_s.split("").map(&:to_i)
   # array.count{ |item| item != array.first} == 0
-  
+  number.to_s.squeeze.length == 1
 end
