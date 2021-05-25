4778fc668ada4c13e62f3a7137c7e6ba4a7ba965
xavierloos <xavier.loos04@gmail.com>
2021-05-25 12:34:01 +0100

    Using UNIQ method

--- Ruby/lib/rep_digit.rb
+++ Ruby/lib/rep_digit.rb
@@ -7,5 +7,8 @@ def repeated_digit number
   raise "Wrong type of argument" if !number.is_a? Numeric
   # array = number.to_s.split("").map(&:to_i)
   # array.count{ |item| item != array.first} == 0
-  number.to_s.squeeze.length == 1
+
+  # number.to_s.squeeze.length == 1
+
+  number.to_s.chars.uniq.length == 1
 end
