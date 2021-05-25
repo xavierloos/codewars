e3394917df523f43ec504f2014a9deb2de024071
xavierloos <xavier.loos04@gmail.com>
2021-05-25 12:50:58 +0100

    Should return the fibonacci

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -8,6 +8,11 @@ describe "fibonacci" do
   it "raises an error if type of argument is different than an integer" do
     expect{ fibonacci "9"}.to raise_error "Wrong type of argument"
   end
+
+  it "shoul return the list of numbers" do
+    expec(fibonacci 9).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21]
+  end
+  
   
   
 end
