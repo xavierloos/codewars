ee93c8b040a3e62af0adb00e13724b7a4b45db51
xavierloos <xavier.loos04@gmail.com>
2021-05-25 12:48:24 +0100

    Rises an error

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -4,5 +4,10 @@ describe "fibonacci" do
   it "should return an array" do
     expect(fibonacci 9).to eq []
   end
+
+  it "raises an error if type of argument is different than an integer" do
+    expect{ fibonacci "9"}.to raise_error "Wrong type of argument"
+  end
+  
   
 end
