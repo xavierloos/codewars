4b0f2b08904ae29365aec0739624eeee322cf9e8
xavierloos <xavier.loos04@gmail.com>
2021-05-26 21:21:31 +0100

    Tests passed

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -5,6 +5,10 @@ describe "fibonacci" do
   #   expect(fibonacci 9).to be_a_kind_of(Array)
   # end
 
+   it "should return an array" do
+    expect(fibonacci 9).to be_a_kind_of(Numeric)
+  end
+
   it "raises an error if type of argument is different than an integer" do
     expect{ fibonacci "9"}.to raise_error "Wrong type of argument"
   end
