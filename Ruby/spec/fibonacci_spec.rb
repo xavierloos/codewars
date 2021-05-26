cafd1753120f27bcc5f63dce58744964eab82f3f
xavierloos <xavier.loos04@gmail.com>
2021-05-26 21:20:23 +0100

    Should return only the number

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -13,6 +13,8 @@ describe "fibonacci" do
   #   expect(fibonacci 9).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21,  34, 55]
   # end
   
-
+  it "shoul return the number" do
+    expect(fibonacci 9).to eq 34
+  end
   
 end
