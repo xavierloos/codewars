465c925f1893a53d09ecf4401407e0a5d4e55cba
xavierloos <xavier.loos04@gmail.com>
2021-05-26 21:10:05 +0100

    Make the if statement

--- Ruby/lib/fibonacci.rb
+++ Ruby/lib/fibonacci.rb
@@ -21,7 +21,7 @@ def fibonacci count
   sequence = []
   (0..count).each do |number|
     sequence << number if number <= 1
-    sequence << sequence[-1] + sequence[-2]
+    sequence << sequence[-1] + sequence[-2] if sequence.length > 1
   end
   sequence
 end
