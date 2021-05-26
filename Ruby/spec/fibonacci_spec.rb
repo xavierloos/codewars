9fc0a1a01f952cda813b9c5f2098c36ef9a2b890
xavierloos <xavier.loos04@gmail.com>
2021-05-26 21:21:03 +0100

    Now the returning result is not a list

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -1,9 +1,9 @@
 require "fibonacci"
 
 describe "fibonacci" do
-  it "should return an array" do
-    expect(fibonacci 9).to be_a_kind_of(Array)
-  end
+  # it "should return an array" do
+  #   expect(fibonacci 9).to be_a_kind_of(Array)
+  # end
 
   it "raises an error if type of argument is different than an integer" do
     expect{ fibonacci "9"}.to raise_error "Wrong type of argument"
