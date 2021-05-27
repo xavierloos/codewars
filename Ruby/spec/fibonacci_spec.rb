84b5379b32cd29f93851f0231f0646fc1fdb9bbe
xavierloos <xavier.loos04@gmail.com>
2021-05-27 21:37:46 +0100

    Update

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -1,12 +1,12 @@
 require "fibonacci"
 
 describe "fibonacci" do
-  
+
   # it "should return an array" do
   #   expect(fibonacci 9).to be_a_kind_of(Array)
   # end
 
-   it "should return an array" do
+  it "should return an array" do
     expect(fibonacci 9).to be_a_kind_of(Numeric)
   end
 
