dc479d8bbf5b77e49743a0ec4f267338ff37010e
xavierloos <xavier.loos04@gmail.com>
2021-05-27 21:31:38 +0100

    Update

--- Ruby/spec/fibonacci_spec.rb
+++ Ruby/spec/fibonacci_spec.rb
@@ -17,6 +17,7 @@ describe "fibonacci" do
   #   expect(fibonacci 9).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21,  34, 55]
   # end
   
+  
   it "shoul return the number" do
     expect(fibonacci 9).to eq 34
   end
