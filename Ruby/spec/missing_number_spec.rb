e456e153090c2086ecbd67c67cdef1b7fd7e76c0
xavierloos <xavier.loos04@gmail.com>
2021-05-28 15:36:39 +0100

    Returns a type of numeric

--- Ruby/spec/missing_number_spec.rb
+++ Ruby/spec/missing_number_spec.rb
@@ -1,5 +1,8 @@
 require "missing_number"
 
 describe "find_missing_number" do
+  it "should return a type of numeric" do
+    expect(find_missing_number [1,4,3,5,2,7,9,6,10]).to be_a_kind_of(Numeric)
+  end
   
 end
