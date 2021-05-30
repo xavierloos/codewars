756c90a6db7afb73ec3a2d64b340a62c038d4725
xavierloos <xavier.loos04@gmail.com>
2021-05-30 10:15:22 +0100

    Update

--- Ruby/spec/missing_number_spec.rb
+++ Ruby/spec/missing_number_spec.rb
@@ -1,7 +1,7 @@
 require "missing_number"
 
 describe "find_missing_number" do
-  
+
   it "should return a type of numeric" do
     expect(find_missing_number [1,4,3,5,2,7,9,6,10]).to be_a_kind_of(Numeric)
   end
@@ -17,4 +17,5 @@ describe "find_missing_number" do
   it "should return a type of numeric" do
     expect(find_missing_number [1,4,3,2,7,9,6,8,10]).to eq 5
   end
+  
 end
