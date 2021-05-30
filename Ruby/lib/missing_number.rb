de78df294f9618563de200e8158914735383b730
xavierloos <xavier.loos04@gmail.com>
2021-05-30 10:15:54 +0100

    List

--- Ruby/lib/missing_number.rb
+++ Ruby/lib/missing_number.rb
@@ -7,7 +7,8 @@
 def find_missing_number(arr)
 # Your code here
   raise "Wrong type of argument" if !arr.is_a? Array
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |item|
+  list =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  list.each do |item|
     found = false
     arr.each do |number|
       if item == number
