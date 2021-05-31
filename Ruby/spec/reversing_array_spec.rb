12b28cbb50f5f20b7028818f2f615c7203b6f626
xavierloos <xavier.loos04@gmail.com>
2021-05-31 14:10:11 +0100

    Should reverse the array

--- Ruby/spec/reversing_array_spec.rb
+++ Ruby/spec/reversing_array_spec.rb
@@ -7,4 +7,7 @@ describe "reverse" do
   it "should raise an error on wrong type of argument" do
     expect{reverse "[1,2,3,4,5]"}.to raise_error "Wrong type of argument"
   end
+  it "should reverse the array" do
+    expect(reverse [1,2,3,4,5]).to eq [5,4,3,2,1]
+  end
 end
