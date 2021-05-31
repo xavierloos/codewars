6c2e0b1ca07ea83f1190117fff47c5cede0f8a9f
xavierloos <xavier.loos04@gmail.com>
2021-05-31 14:08:47 +0100

    Test: passed

--- Ruby/lib/reversing_array.rb
+++ Ruby/lib/reversing_array.rb
@@ -3,5 +3,6 @@
 # [1,2,3,4,5] => [5,4,3,2,1]
 
 def reverse(arr)
+  raise "Wrong type of argument" if !arr.is_a? Array
   []
 end
--- Ruby/spec/reversing_array_spec.rb
+++ Ruby/spec/reversing_array_spec.rb
@@ -5,6 +5,6 @@ describe "reverse" do
     expect(reverse [1,2,3,4,5]).to be_a_kind_of(Array)
   end
   it "should raise an error on wrong type of argument" do
-    expect{reverse [1,2,3,4,5]}to raise_error "Wrong type of argument"
+    expect{reverse "[1,2,3,4,5]"}.to raise_error "Wrong type of argument"
   end
 end
