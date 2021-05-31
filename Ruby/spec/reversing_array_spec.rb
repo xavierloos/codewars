39d9e2a519d30819326c6e362a0d9df5e8fb0ad4
xavierloos <xavier.loos04@gmail.com>
2021-05-31 14:01:36 +0100

    Test: passed

--- Ruby/spec/reversing_array_spec.rb
+++ Ruby/spec/reversing_array_spec.rb
@@ -2,6 +2,6 @@ require "reversing_array"
 
 describe "reverse" do
   it "should return an array" do
-    expect(fibonacci [1,2,3,4,5]).to be_a_kind_of(Array)
+    expect(reverse [1,2,3,4,5]).to be_a_kind_of(Array)
   end
 end
