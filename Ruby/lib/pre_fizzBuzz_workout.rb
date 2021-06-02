66a452eb7861bb91b3dcc1b3ff4459d0eaf3c8fe
xavierloos <xavier.loos04@gmail.com>
2021-06-02 14:00:32 +0100

    Update

--- Ruby/lib/pre_fizzBuzz_workout.rb
+++ Ruby/lib/pre_fizzBuzz_workout.rb
@@ -2,7 +2,6 @@
 # Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
 # Your expected output is an array of positive integers from 1 to n (inclusive).
 # Your job is to write an algorithm that gets you from the input to the output.
-
 def pre_fizz(n)
   arr = []
   1.upto(n) do |i|
