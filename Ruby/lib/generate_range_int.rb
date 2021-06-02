d781f3d6913d1cf7bd27cb6ba3aab301f8fb3cea
xavierloos <xavier.loos04@gmail.com>
2021-06-02 13:58:14 +0100

    Update

--- Ruby/lib/generate_range_int.rb
+++ Ruby/lib/generate_range_int.rb
@@ -1,7 +1,6 @@
 # https://www.codewars.com/kata/55eca815d0d20962e1000106/train/ruby
 
 # Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
-
 # Task
 # Implement a function named
 
