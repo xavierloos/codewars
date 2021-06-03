932128a59d42ad96d038bbf8d72ab128235a5eee
xavierloos <xavier.loos04@gmail.com>
2021-06-03 16:59:24 +0100

    comment

--- Ruby/lib/greater_than.rb
+++ Ruby/lib/greater_than.rb
@@ -1,9 +1,10 @@
-# Create a function that get 2 arguments (the number you want to check, and the list of numbers you want to evaluate.) to see how many times is greater than the numbers passed.
+=begin
+Create a function that get 2 arguments (the number you want to check, and the list of numbers you want to evaluate.) to see how many times is greater than the numbers passed.
 
-# Example
-# Input                   Output
-# 5, [5,6,7,1,2] => 2 (5 is not greater than 5 so it does not count)
-
+Example
+Input                   Output
+ 5, [5,6,7,1,2] => 2 (5 is not greater than 5 so it does not count)
+=end
 def greater_than number, array
   # Your code here
   raise "Invalid number or negative" if !number.is_a? Numeric or number <=0
