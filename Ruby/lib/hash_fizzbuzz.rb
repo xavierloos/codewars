04143e5435328390bbbd5166aa7ae0dee2df2da7
xavierloos <xavier.loos04@gmail.com>
2021-06-03 17:00:04 +0100

    Multiline comment

--- Ruby/lib/hash_fizzbuzz.rb
+++ Ruby/lib/hash_fizzbuzz.rb
@@ -1,16 +1,17 @@
-# Create the fizzbuzz example but with a little bit of twist
-# The function should return a hash with the number as a key and the result as a value.
+=begin
+Create the fizzbuzz example but with a little bit of twist
+The function should return a hash with the number as a key and the result as a value.
 
-# Example
-# {
-#   1=>1,
-#   2=>2,
-#   3=>Fizz,
-#   4=>4,
-#   5=>Buzz,
-#   etc.
-# }
-
+Example
+{
+  1=>1,
+  2=>2,
+  3=>Fizz,
+  4=>4,
+  5=>Buzz,
+  etc.
+}
+=end
 def fizzbuzz num
   # Your code here
   raise "Input invalid" if num < 1 
